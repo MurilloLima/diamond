1967b0ff360015bce2366baa05d7058825e114a9
Murillo Lima <murillo_smit@hotmail.com>
2025-07-06 08:46:45 -0300

    footer

--- resources/views/admin/pages/diamond/index.blade.php
+++ resources/views/admin/pages/diamond/index.blade.php
@@ -234,30 +234,7 @@
                 </div>
             </div>
 
-            <footer class="footer">
-                <div class="container-fluid d-flex justify-content-between">
-                    <nav class="pull-left">
-                        <ul class="nav">
-                            <li class="nav-item">
-                                <a class="nav-link" href="">
-                                    Logo marcar
-                                </a>
-                            </li>
-                            {{-- <li class="nav-item">
-                                <a class="nav-link" href="#"> Help </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#"> Licenses </a>
-                            </li> --}}
-                        </ul>
-                    </nav>
-                    {{-- <div class="copyright">
-                        2025, Desenvolvido <i class="fa fa-heart heart text-danger"></i> por
-                        <a href="http://www.murillolimadev.com.br">Murillo Lima</a>
-                    </div> --}}
-
-                </div>
-            </footer>
+            @include('admin.layouts.footer')
         </div>
 
         <!-- Custom template | don't include it in your project! -->
